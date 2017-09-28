d94fc2ffd423ed2f917e6bafe5ee7fccb39f309d
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-28 01:53:38 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -120,8 +120,6 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holidays|
     #binding.pry
     bbq_holidays = holidays.select { |k,v| v =="BBQ" }
-
-
   end
   bbq_holidays
 end
