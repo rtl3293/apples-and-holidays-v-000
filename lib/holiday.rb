0e3061e971f62d95e1e6bf7e61a0aa163711e75a
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-28 01:34:45 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -116,8 +116,5 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  
+
 end
-
-
-all_supplies_in_holidays(holiday_hash)
